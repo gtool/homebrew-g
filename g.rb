df416a0efab611722301ecb87aa50bf8fccffbeb
Vasily Polovnyov <vasily@polovnyov.ru>
2015-09-03 23:26:42 +0300

    1.0.1

--- g.rb
+++ g.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class G < Formula
   homepage "https://github.com/gtool/g"
-  version "1.0.0"
+  version "1.0.1"
 
-  url "https://github.com/gtool/g/archive/v1.0.0.zip"
-  sha1 "3753a1f9068270504ee661945049c71116ff99a2"
+  url "https://github.com/gtool/g/archive/v1.0.1.zip"
+  sha1 "f9a3e525b2f6b9648bd7a67db3c2a69316359a6f"
 
   def install
     bin.install Dir['bin/**']
